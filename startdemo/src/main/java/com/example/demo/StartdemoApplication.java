db21bbdf6793951b64fca9cb126a72bb5830bff7
zhangjin185 <zhangjinhappy>
2020-04-20 00:05:59 +0800

    微服务的开始旅程

---
+++ startdemo/src/main/java/com/example/demo/StartdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartdemoApplication.class, args);
+	}
+
+}
